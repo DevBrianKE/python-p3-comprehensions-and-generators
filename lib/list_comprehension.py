215e9589ed5a6d7ae951f7b951fd6a07105c4577
DevBrianKE <kipchumbabrian47@gmail.com>
2025-05-07 15:05:18 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -5,5 +5,4 @@ def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-
-    return [(letter + "!") for letter in sentence_list]
+    return [sentence + "!" for sentence in sentence_list]
